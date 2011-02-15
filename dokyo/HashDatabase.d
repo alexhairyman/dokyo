86fff00d594656f3b0983952dd3e1b44c7d76560
Dmitry A. Ustalov <dmitry@eveel.ru>
2011-02-16 03:50:25 +0500

    HashDatabase: major rewrite with awesomeness

--- dokyo/HashDatabase.d
+++ dokyo/HashDatabase.d
@@ -1,7 +1,11 @@
 module dokyo.HashDatabase;
 
-private import dokyo.capi.tchdb;
 private import tango.stdc.stringz;
+private import tango.core.Exception :
+    PlatformException, NoSuchElementException;
+
+private import dokyo.capi.tchdb;
+private import dokyo.capi.tcutil;
 
 /**
   * Object-Oriented Wrapper for Tokyo Cabinet
@@ -11,74 +15,128 @@ private import tango.stdc.stringz;
   *
   * TODO:
   *   1) foreach support
-  *   2) exceptions instead of crappy ecode returns
-  *   3) more container methods like HashMap
-  *   4) code documentation
+  *   2) more container methods like HashMap
+  *   3) iterators
   */
 class HashDatabase
 {
     /* Tokyo Cabinet Hash Database */
     private TCHDB *hdb = null;
 
-    this(char[] filename)
+    /**
+      * Create a new instance of HashDatabase, which will
+      * open database in specified filename
+      * with defined mode (default: read/write + create).
+      */
+    this(char[] filename, int mode = HDBOWRITER | HDBOCREAT)
     {
         hdb = tchdbnew();
 
-        int ecode;
-        if(!tchdbopen(hdb, &filename[0], HDBOWRITER | HDBOCREAT)) {
-            ecode = tchdbecode(hdb);
+        char *zfilename = toStringz(filename);
+        if (!tchdbopen(hdb, zfilename, mode)) {
+            int ecode = tchdbecode(hdb);
+            throw new PlatformException(getError(ecode));
         }
+        delete zfilename;
     }
 
     ~this()
     {
-        reset();
-    }
-
-    public char[] get(char[] key)
-    {
-        char *value = tchdbget2(hdb, &key[0]);
-
-        int ecode;
-        if (value)
+        if (!tchdbclose(hdb))
         {
-            // actually I don't know how Tokyo Cabinet
-            // behaves with allocated values
-            return fromStringz(value);
+            int ecode = tchdbecode(hdb);
+            throw new PlatformException(getError(ecode));
         }
-        else
-        {
-            ecode = tchdbecode(hdb);
-        }
-    }
-
-    public int set(char[] key, char[] value)
-    {
-        if (!tchdbput2(hdb, &key[0], &value[0]))
-        {
-            return tchdbecode(hdb);
-        }
-        else
-        {
-            return 0;
-        }
-    }
-
-    private final HashDatabase reset()
-    {
         tchdbdel(hdb);
         hdb = null;
-        return this;
     }
 
-    public final HashDatabase clear()
+    /**
+      * Retrieve some record from database by defined key.
+      * Returns null if there is no record and throws
+      * PlatformException at any other error.
+      */
+    final char[] get(char[] key)
+    {
+        char *zkey = toStringz(key);
+        char *zvalue = tchdbget2(hdb, zkey);
+        delete zkey;
+
+        if (!zvalue)
+        {
+            int ecode = tchdbecode(hdb);
+            switch (ecode)
+            {
+                case TCENOREC: return null;
+                default:
+                {
+                    char[] errmsg = getError(ecode);
+                    throw new PlatformException(errmsg);
+                }
+            }
+        }
+
+        char[] value = fromStringz(zvalue);
+        delete zvalue;
+        return value;
+    }
+
+    alias get opIndex;
+
+    /**
+      * Store value in database with defined key. Always
+      * returns true. When error occurs, throws
+      * PlatformException.
+      */
+    final bool set(char[] key, char[] value)
+    {
+        char *zkey = toStringz(key);
+        char *zvalue = toStringz(value);
+
+        bool success = tchdbput2(hdb, zkey, zvalue);
+        delete zkey;
+        delete zvalue;
+
+        if (!success)
+        {
+            int ecode = tchdbecode(hdb);
+            throw new PlatformException(getError(ecode));
+        }
+
+        return true;
+    }
+
+    final bool opIndexAssign(char[] value, char[] key)
+    {
+        return set(key, value);
+    }
+
+    /**
+      * Remove anything from database.
+      */
+    final HashDatabase clear()
     {
         tchdbvanish(hdb);
         return this;
     }
 
-    public final int size()
+    /**
+      * Number of records, stored in this database.
+      */
+    final size_t size()
     {
         return tchdbrnum(hdb);
+      }
+
+    /**
+      * Read an error message description from specified
+      * error code and convert into string.
+      */
+    private char[] getError(int ecode)
+    {
+        char *zerrmsg = tchdberrmsg(ecode);
+        char[] errmsg = fromStringz(zerrmsg);
+        delete zerrmsg;
+        return errmsg;
     }
 }
