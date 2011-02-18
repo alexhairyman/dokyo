24bca4ab65f30988b380fab9a32a576f7689dabd
Dmitry A. Ustalov <dmitry@eveel.ru>
2011-02-18 22:09:39 +0500

    HashDatabaseExample: foreach demo

--- example/HashDatabaseExample.d
+++ example/HashDatabaseExample.d
@@ -2,8 +2,6 @@
  * Example of using dokyo.HashDatabase class to
  * perform common tasks with Tokyo Cabinet
  * Hash Database.
- *
- * Also used as test suite, sorry guys.
  */
 
 module dokyo.HashDatabaseExample;
@@ -20,35 +18,35 @@ void main()
     Stdout.formatln("db is cleaned up");
 
     // size()
-    assert(db.size() == 0);
     Stdout.formatln("db is empty");
 
     // get() non-existent key
-    assert(db["foo"] == null);
-    assert(db.get("foo") == null);
     Stdout.formatln("db[\"{}\"] is {}", "foo", "null");
 
     // set() once
     db["foo"] = "bar";
-    assert(db["foo"] == "bar");
-    assert(db.get("foo") == "bar");
-    assert(db.size() == 1);
     Stdout.formatln("db[\"{}\"] is {}", "foo", db["foo"]);
     Stdout.formatln("db size is {}", db.size());
 
     // set() again
     db.set("bar", "baz");
-    assert(db["bar"] == "baz");
-    assert(db.get("bar") == "baz");
-    assert(db.size() == 2);
     Stdout.formatln("db[\"{}\"] is {}", "bar", db["bar"]);
     Stdout.formatln("db size is {}", db.size());
 
+    // foreach
+    foreach (key; db)
+    {
+        Stdout.formatln("foreach(k): \"{}\"", key);
+    }
+
+    // double foreach
+    foreach (key, value; db)
+    {
+        Stdout.formatln("foreach(k, v): \"{}\" is \"{}\"", key, value);
+    }
+
     // remove()
     db.remove("bar");
-    assert(db["bar"] == null);
-    assert(db.get("bar") == null);
-    assert(db.size() == 1);
     Stdout.formatln("db[\"{}\"] is {}", "bar", "null");
     Stdout.formatln("db size is {}", db.size());
 
